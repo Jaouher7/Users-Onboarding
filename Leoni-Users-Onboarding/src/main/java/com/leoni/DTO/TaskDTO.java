8ca8676e72f92c3f8e6dae46996bb2ddaa18575e
jawher <jawhertabbabi99@gmail.com>
2024-05-11 20:41:40 +0100

    origin master

---
+++ Leoni-Users-Onboarding/src/main/java/com/leoni/DTO/TaskDTO.java
@@ -0,0 +1,54 @@
+package com.leoni.DTO;
+
+public class TaskDTO {
+
+    private String username;
+
+    private Long designId;
+
+    private String state;
+
+    private String priority;
+
+    public TaskDTO() {
+    }
+
+    public TaskDTO(String username, Long designId, String state, String priority) {
+        this.username = username;
+        this.designId = designId;
+        this.state = state;
+        this.priority = priority;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getDesignId() {
+        return designId;
+    }
+
+    public void setDesignId(Long designId) {
+        this.designId = designId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
